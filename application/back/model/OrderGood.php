3ed46261a232c79e1ff1737eefa682bdd5d27a2c
zygals <2799024884@qq.com>
2017-10-19 16:26:35 +0800

    kuangjia update

--- application/back/model/OrderGood.php
+++ application/back/model/OrderGood.php
@@ -11,15 +11,15 @@ class OrderGood extends model {
     const ST_RETURN = 4;
 
     public static function getGood($order_id) {
-        $where = ['order_id' => $order_id,'good.st'=>1];
 
+        $where = ['order_id' => $order_id,'good.st'=>1];
         $list_ = self::where($where)->alias('od')->join('good','od.good_id=good.id')->field('od.*,good.name good_name,good.price,good.img')->order('od.create_time asc')->select();
 
         return $list_;
     }
 
     public static  function getGoodOn($good_id){
-        $list_ = self::where('(good_id=:good_id and st=1 ) or (good_id=:good_id and st=2)')->bind(['good_id'=>$good_id])->fetchSql()->find();
+        $list_ = self::where("(good_id=$good_id and st=1 ) or (good_id=$good_id and st=2)")->fetchSql(false)->find();
       //dump($list_);exit;
         return $list_;
     }
