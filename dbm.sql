6034c9cda9167e84a15fc6a9b4a8a9fa00c5d8ef
mazhida <138766778@qq.com>
2017-10-25 09:25:55 +0800

    10-24gai

---
+++ dbm.sql
@@ -0,0 +1,21 @@
+ALTER TABLE `zhuangxiu`.`withdraw` 
+CHANGE COLUMN `cach` `cash` decimal(8, 2) NOT NULL DEFAULT 0.00 COMMENT '申请提现金额' AFTER `admin_id`;
+
+
+ALTER TABLE `zhuangxiu`.`withdraw` 
+MODIFY COLUMN `cash_st` tinyint(4) NOT NULL DEFAULT 1 COMMENT '资金状态：0待转账 1转账成功 2转账失败 ' AFTER `st`;
+
+ALTER TABLE `zhuangxiu`.`withdraw` 
+MODIFY COLUMN `cash_st` tinyint(4) NOT NULL DEFAULT 0 COMMENT '资金状态：0待转账 1转账成功 2转账失败 ' AFTER `st`;
+
+ALTER TABLE `zhuangxiu`.`collect` 
+CHANGE COLUMN `good_id` `collect_id` int(11) NOT NULL AFTER `user_id`;
+
+ALTER TABLE `zhuangxiu`.`collect` 
+ADD COLUMN `type` tinyint(4) NOT NULL COMMENT '1收藏商品id 2收藏店铺id' AFTER `update_time`;
+
+ALTER TABLE `zhuangxiu`.`address` 
+ADD COLUMN `st` tinyint(4) NOT NULL DEFAULT 1 COMMENT '0删除 1正常' AFTER `update_time`;
+
+ALTER TABLE `zhuangxiu`.`good` 
+ADD COLUMN `unit` varchar(30) NOT NULL COMMENT '计量单位' AFTER `to_top`;
