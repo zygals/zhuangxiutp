8858a521bcc0be1acb6791127f51e7fbb379ec35
j28981 <2799024884@qq.com>
2017-10-31 11:22:27 +0800

    api/dingdan/gotopay-5

--- application/api/model/Dingdan.php
+++ application/api/model/Dingdan.php
@@ -190,7 +190,7 @@ class Dingdan extends Base{
 	public function getOrder($data){
 		$order_id = $data['order_id'];
 		$row_order = self::where(['dingdan.id'=>$order_id])->join('
-		shop','shop.id=order.shop_id')->field('dingdan.*,shop.name shop_name')->find( $order_id );
+		shop','shop.id=dingdan.shop_id')->field('dingdan.*,shop.name shop_name')->find();
 		if ( !$row_order ) {
 			return ['code' => __LINE__ , 'msg' => 'order is not exists'];
 		}
