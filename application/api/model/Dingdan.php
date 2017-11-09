f6dc3786f30bc737d97254b64eb82f509b89a934
j28981 <2799024884@qq.com>
2017-11-09 10:59:30 +0800

    dingan gai

--- application/api/model/Dingdan.php
+++ application/api/model/Dingdan.php
@@ -99,7 +99,7 @@ class Dingdan extends Base{
 		if ( is_array( $user_id ) ) {
 			return $user_id;
 		}
-		if ( $row = self::where( ['user_id' => $user_id , 'type' => $data['type_'] , 'shop_id' => $data['shop_id']] )->find() ) {
+		if ( $row = self::where( ['user_id' => $user_id , 'type' => $data['type_'] , 'shop_id' => $data['shop_id'],'st'=>1] )->find() ) {
 			return ['code' => __LINE__ , 'msg' => '不能重复添加'];
 		}
 		$data_order['shop_id'] = $data['shop_id'];
@@ -159,7 +159,7 @@ class Dingdan extends Base{
 			return $user_id;
 		}
 		//如果已添加不重复
-		if ( $row = self::where( ['user_id' => $user_id , 'type' => $data['type_'] , 'group_id' => $data['t_id']] )->find() ) {
+		if ( $row = self::where( ['user_id' => $user_id , 'type' => $data['type_'] , 'group_id' => $data['t_id'],'st'=>1] )->find() ) {
 			return ['code' => __LINE__ , 'msg' => '不能重复添加'];
 		}
 		//dump($row);exit;
