cdfe855fd49fa6117ac1b5269858e0845c98019b
j28981 <2799024884@qq.com>
2017-11-10 03:52:41 +0800

    gai7

--- application/api/model/Dingdan.php
+++ application/api/model/Dingdan.php
@@ -420,7 +420,7 @@ class Dingdan extends Base{
 			//将用户团购订金订单取消
 			$user_id=User::getUserIdByName($data['username']);
 			if($data['type_'] == Dingdan::ORDER_TYPE_GROUP_FINAL){
-               self::where(['user_id'=>$user_id,'type'=>self::ORDER_TYPE_GROUP_DEPOSIT,'group_id'=>$row_order->group_id])->update(['st'=>self::ORDER_ST_USER_CANCEL]);
+               self::where(['user_id'=>$user_id,'type'=>self::ORDER_TYPE_GROUP_DEPOSIT,'group_id'=>$row_order->group_id])->save(['st'=>self::ORDER_ST_USER_CANCEL]);
 			}
 			return ['code' => 0 , 'msg' => '订单为已支付'];
 		} elseif ( $data['type_'] == Dingdan::ORDER_TYPE_CONTACT ) {
