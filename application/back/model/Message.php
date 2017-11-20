abd61382f6406b7873cd685fe24a136eaff4efbd
zygals <2799024884@qq.com>
2017-11-20 18:02:06 +0800

    message back 3

--- application/back/model/Message.php
+++ application/back/model/Message.php
@@ -16,7 +16,7 @@ class Message extends Base{
     public static function getList($where=['message.status' => ['<>',0]]){
        $order = 'message.create_time desc';
         $fields = 'message.*,user.nickname,user.id user_id,user.username ,shop.name shop_name';
-        $list_ = self::where($where)->join('shop','shop.id=message.shop_id')->join('user','user.id=message.user_id')->field($fields)->order($order)->group('message.id,message.user_id,message.shop_id')->paginate(10);
+        $list_ = self::where($where)->join('shop','shop.id=message.shop_id')->join('user','user.id=message.user_id')->field($fields)->order($order)->group('message.user_id,message.shop_id')->paginate(10);
         return $list_;
     }
 
