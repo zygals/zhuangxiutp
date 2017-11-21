ccccbf464b37fcf89b85fcbefa797b3c9a0b6f60
zygals <2799024884@qq.com>
2017-11-21 10:59:53 +0800

    back

--- application/back/view/message/info.php
+++ application/back/view/message/info.php
@@ -5,9 +5,21 @@
     .control-label {
         padding-right: 10px;
     }
+    .from-user {
 
+    }
     .from-me {
         text-align: right;
+    }
+    .msgs_div{
+
+    }
+    .msglist{
+
+    }  .huifu{
+
+    }  .pagediv{
+
     }
 </style>
 <div class="row msgs_div">
@@ -15,7 +27,7 @@
         <?php foreach ($list_ as $k => $msg) { ?>
             <p class="<?php echo $msg->type == '1' ? 'from-user' : 'from-me'; ?>">
                 <?php if ($msg->type == 1) { ?>{$msg->user_id}:{$msg->username} ({$msg->nickname})<?php } else { ?>{$msg->shop_id}:{$msg->shop_name}<?php } ?>
-                ：{$msg->message} 「{$msg->create_time}」
+                ：{$msg->message} 「{$msg->create_time}」<button class="delmsg btn btn-danger btn-xs">删除</button>
             </p>
         <?php } ?>
     </div>
