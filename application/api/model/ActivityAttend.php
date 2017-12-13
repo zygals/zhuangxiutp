52b0bb5eb58245937fb5ef1d96b0f1135a4533c0
zygals <2799024884@qq.com>
2017-12-13 09:44:03 +0800

    tplmsg 2

--- application/api/model/ActivityAttend.php
+++ application/api/model/ActivityAttend.php
@@ -15,7 +15,7 @@ class ActivityAttend extends Base {
         if (is_array($user_id)) {
             return $user_id;
         }
-        $list_ =self::where(['user_id'=>$user_id])->field("activity_attend.*, FROM_UNIXTIME( time_to, '%Y-%m-%d %H:%i:%S' ) time_to,activity.name activity_name,activity.address activity_address,activity.img activity_img,activity.type")->join('activity','activity.id=activity_attend.activity_id')->select() ;
+        $list_ =self::where(['user_id'=>$user_id])->field("activity_attend.*, FROM_UNIXTIME( time_to, '%Y-%m-%d %H:%i:%S' ) time_to,activity.name activity_name,activity.address activity_address,activity.img activity_img,activity.type,activity.img_big img_big")->join('activity','activity.id=activity_attend.activity_id')->select() ;
         if($list_->isEmpty()){
 return ['code'=>__LINE__];
         }
