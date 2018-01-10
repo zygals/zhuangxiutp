08a338858875982afb99c36e0a7cb6a5819dd611
zygals <2799024884@qq.com>
2018-01-10 11:13:37 +0800

    back

--- application/back/controller/WithdrawController.php
+++ application/back/controller/WithdrawController.php
@@ -39,7 +39,7 @@ class WithdrawController extends BaseController{
      * @param Request $request
      */
     public function save(Request $request){
-    
+
         $data = $request->param();
         $rule =['cash'=>'require|number'];
         $res=$this->validate($data,$rule);
@@ -57,7 +57,8 @@ class WithdrawController extends BaseController{
         $data['admin_id']= session('admin_zhx')->id;
         //提现５００，实际确认收货的也是500，则可以提现，如此时提现６００则不能提现，提示失败！
         $confirm_order = Dingdan::getConfirmOrderSum($data['admin_id']);
-        if($data['cash'] + $remian['already_apply'] > $confirm_order){
+        $withdraw_ok=Admin::where(['id'=>$data['admin_id']])->value('withdraw_ok');
+        if($data['cash'] + $remian['already_apply'] > $confirm_order-$withdraw_ok){
             $this->error("提现金额超过已收货的金额({$confirm_order} 元)，申请失败");
         }
         if($data['cash'] > $remian['remain']){
