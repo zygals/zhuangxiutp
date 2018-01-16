78635dc6f578de828d9c3112790c0b43e1ec1403
zygals <2799024884@qq.com>
2018-01-16 12:52:12 +0800

    api

--- public/notify.php
+++ public/notify.php
@@ -4,7 +4,7 @@ $fc = file_get_contents("php://input");
 $xml = simplexml_load_string($fc);
 $fp = fopen('xml.txt', 'a');
 
-$str = 'appid:' . (string)$xml->appid . "return_code:" . (string)$xml->return_code . "result_code:" . (string)$xml->result_code . 'is_string:' . is_string($fc) .'-time_end'.(string)$xml->time_end. 'out_trade_no'.(string)$xml->out_trade_no."\n";
+$str = 'appid:' . (string)$xml->appid . "return_code:" . (string)$xml->return_code . "result_code:" . (string)$xml->result_code . 'is_string:' . is_string($fc) .'-time_end'.(string)$xml->time_end. 'out_trade_no'.(string)$xml->out_trade_no.'-sign'.(string)$xml->sign."\n";
 fwrite($fp, $str);
 echo "<xml>
    <return_code><![CDATA[SUCCESS]]></return_code>
