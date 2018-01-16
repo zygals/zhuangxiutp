1e1dfc9ca06133ed41e950520936038bec5b9071
zygals <2799024884@qq.com>
2018-01-16 13:22:02 +0800

    api

--- application/api/controller/DingdanController.php
+++ application/api/controller/DingdanController.php
@@ -254,14 +254,13 @@ class DingdanController extends BaseController {
 
     }
 
-    public function notify_($fc=''){
+    public function notify_($weixin_notify=''){
         //echo 'nogify_';exit;
         $fp = fopen('xml.txt', 'a');
-        $str= fread($fp,300);
-        dump($fp);exit;
-        $xml = simplexml_load_string($fc);
 
-        $str = 'appid:' . (string)$xml->appid . "return_code:" . (string)$xml->return_code . "result_code:" . (string)$xml->result_code . 'is_string:' . is_string($fc) .'-time_end'.(string)$xml->time_end. 'out_trade_no'.(string)$xml->out_trade_no.'-sign'.(string)$xml->sign."\n";
+        $xml = simplexml_load_string($weixin_notify);
+
+        $str = 'appid:' . (string)$xml->appid . "return_code:" . (string)$xml->return_code . "result_code:" . (string)$xml->result_code . 'is_string:' . is_string($weixin_notify) .'-time_end'.(string)$xml->time_end. 'out_trade_no'.(string)$xml->out_trade_no.'-sign'.(string)$xml->sign."\n";
         fwrite($fp, $str);
         echo "<xml>
    <return_code><![CDATA[SUCCESS]]></return_code>
--- public/notify.php
+++ public/notify.php
@@ -1,9 +1,28 @@
 <?php
 
+function http_request($url, $data = null, $headers = array()) {
+    $curl = curl_init();
+    if (count($headers) >= 1) {
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+    }
+    curl_setopt($curl, CURLOPT_URL, $url);
+
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE);
+
+    if (!empty($data)) {
+        curl_setopt($curl, CURLOPT_POST, 1);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+    }
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    $output = curl_exec($curl);
+    curl_close($curl);
+    return $output;
+}
 
 // pay nodify_url
-$fc = file_get_contents("php://input");
+$weixin_notify = file_get_contents("php://input");
 
-$url = "https://huahui.qingyy.net/zhuangxiutp/public/index.php/api/dingdan/notify_?fc=$fc";
+$url = "https://huahui.qingyy.net/zhuangxiutp/public/index.php/api/dingdan/notify_";
 
-
+http_request($url,"{weixin_notify:$weixin_notify}");
