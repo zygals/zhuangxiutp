604cd772ecba085cdf6e1762b133d611d6682a24
zygals <2799024884@qq.com>
2018-01-17 11:41:06 +0800

    api

--- application/api/model/TplMessage.php
+++ application/api/model/TplMessage.php
@@ -28,7 +28,7 @@ class TplMessage extends Base {
             'touser'=>$user->open_id,
             "template_id"=>config('template_id_payok'),
             "form_id"=>$prepay_id,
-            "page"=>'pages/orders/orders',
+            "page"=>'pages/index/index',
             "data"=>[
                 'keyword1'=>[
                     "value"=> $row_order->orderno,
