24fccd3057408220331ccb760dcedb14aeff229c
zygals <2799024884@qq.com>
2018-01-18 13:57:20 +0800

    api

--- application/api/model/Dingdan.php
+++ application/api/model/Dingdan.php
@@ -429,7 +429,7 @@ class Dingdan extends Base{
         $row_order = self::where(['orderno' => $xmlobj->out_trade_no])->find();
         if($row_order){
            if($row_order->sum_price*100 != $xmlobj->cash_fee){
-                fwrite($fp, $row_order->orderno."=> cash_fee error {$row_order->sum_price*100}_ $xmlobj->cash_fee\n");
+                fwrite($fp, $row_order->orderno."=> cash_fee error {$row_order->sum_price}_ $xmlobj->cash_fee\n");
                 return  "<xml>
                    <return_code><![CDATA[FAIL]]></return_code>
                    <return_msg><![CDATA[cash_fee error]]></return_msg>
