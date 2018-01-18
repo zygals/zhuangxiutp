0c23bcae35f216828302372363e8aa9fe3e1c5f8
zygals <2799024884@qq.com>
2018-01-18 15:14:57 +0800

    back

--- application/back/view/group/index.php
+++ application/back/view/group/index.php
@@ -101,8 +101,8 @@
 						<div class="col-xs-1 ">
 							{$row_->id}
 						</div>
-						<div class="col-xs-1 " title="{$row_->shop_id}:{$row_->shop_name}">
-							{$row_->shop_id}:{$row_->shop_name}{$row_->getShopSt($row_->shop_st)}
+						<div class="col-xs-1 " title="{$row_->shop_id}:{$row_->shop_name}-{$row_->getShopSt($row_->shop_st)">
+							{$row_->shop_id}:{$row_->shop_name}-{$row_->getShopSt($row_->shop_st)}
 						</div>
 						<div class="col-xs-1 " title="{$row_->good_name}">
 							{$row_->good_name}
