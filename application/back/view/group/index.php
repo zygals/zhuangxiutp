0e05404a2887cdfb250d04e6717e4831d044f482
zygals <2799024884@qq.com>
2018-01-18 15:18:08 +0800

    back

--- application/back/view/group/index.php
+++ application/back/view/group/index.php
@@ -102,7 +102,11 @@
 							{$row_->id}
 						</div>
 						<div class="col-xs-1 " title="{$row_->shop_id}:{$row_->shop_name}-{$row_->getShopSt($row_->shop_st)}">
-							{$row_->shop_id}:{$row_->shop_name}-{$row_->getShopSt($row_->shop_st)}
+                            <?php if($row_->shop_st==0 || $row_->shop_st==2){?>
+							<span style="color:red">{$row_->shop_id}:{$row_->shop_name}-{$row_->getShopSt($row_->shop_st)}</span>
+<?php }else{?>
+                                {$row_->shop_id}:{$row_->shop_name}-{$row_->getShopSt($row_->shop_st)}
+                        <?php }?>
 						</div>
 						<div class="col-xs-1 " title="{$row_->good_name}">
 							{$row_->good_name}
